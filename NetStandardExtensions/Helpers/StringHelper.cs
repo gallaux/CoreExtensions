63a03168bea8823b2e9a36a62f073200170e3d70
Guillaume Allaux <gallaux87@gmail.com>
2022-04-20 14:17:27 -0500

    Added slice method to String

--- NetStandardExtensions/Helpers/StringHelper.cs
+++ NetStandardExtensions/Helpers/StringHelper.cs
@@ -16,5 +16,18 @@ namespace NetStandardExtensions.Helpers
         {
             return value == null ? value : Regex.Replace(value, "\\u00a0", " ");
         }
+
+        /// <summary>
+        /// Extracts a portion of a string, selected from start index to end index
+        /// Similar to JavaScript's string.slice() method
+        /// </summary>
+        /// <param name="value">String to slice</param>
+        /// <param name="startIndex">The start index</param>
+        /// <param name="endIndex">The end index</param>
+        /// <returns>Sliced substring</returns>
+        public static string Slice(string value, int startIndex, int? endIndex = null)
+        {
+            return value.Substring(startIndex, value.Length - (endIndex ?? startIndex));
+        }
     }
 }
--- NetStandardExtensionsTests/Tests/StringTests.cs
+++ NetStandardExtensionsTests/Tests/StringTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetStandardExtensions.Extensions;
 using NetStandardExtensions.Helpers;
+using System;
 
 namespace NetStandardExtensionsTests
 {
@@ -33,6 +34,49 @@ namespace NetStandardExtensionsTests
             
             // Extension
             Assert.AreEqual(expected, nullStr.ReplaceNonBreakingSpaces());
+        }
+
+        [TestMethod]
+        public void String_Slice_ReturnSlicedString_FromStartToEndIndex()
+        {
+            var expected = "loWo";
+            var str = "helloWorld";
+
+            // Helper
+            Assert.AreEqual(expected, StringHelper.Slice(str, 3, 6));
+
+            // Extension
+            Assert.AreEqual(expected, str.Slice(3, 6));
+        }
+
+        [TestMethod]
+        public void String_Slice_ReturnsSlicedString_FromStartIndexToEndOfString()
+        {
+            var expected = "loWorld";
+            var str = "helloWorld";
+
+            // Helper
+            Assert.AreEqual(expected, StringHelper.Slice(str, 3));
+
+            // Extension
+            Assert.AreEqual(expected, str.Slice(3));
+        }
+
+        [TestMethod]
+        public void String_Slice_ThrowsArgumentOutOfRangeException_IfIndexOutOfBound()
+        {
+            var str = "helloWorld";
+
+            Assert.ThrowsException<ArgumentOutOfRangeException> (() => StringHelper.Slice(str, -3));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => StringHelper.Slice(str, 3, 20));
+        }
+
+        [TestMethod]
+        public void String_Slice_ThrowsNullReferenceException_IfStringIsNull()
+        {
+            var str = null as string;
+
+            Assert.ThrowsException<NullReferenceException>(() => StringHelper.Slice(str, 3));
         }
     }
 }
